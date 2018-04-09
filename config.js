3e9fd52bce22fbf51eafd0d7f00712d799415310
thib_rdr <thibaudroudier@gmail.com>
2018-04-09 20:21:21 +0200

    add logger, dotenv config

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    database: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    },
+
+    server: {
+        port: process.env.SERVER_PORT,
+    },
+
+    rateLimit: {
+        total: process.env.RATE_LIMIT_TOTAL,
+        expire: process.env.RATE_LIMIT_EXPIRE,
+    },
+
+    log: {
+        level: process.env.LOG_LEVEL,
+        dateformat: process.env.LOG_DATE_FORMAT
+    },
+
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRE_IN,
+        audience: process.env.JWT_AUDIENCE,
+        issuer: process.env.JWT_ISSUER,
+    },
+};
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config();
+}
+
+const config = require('./config.js');
 const express = require('express')
 const app = express()
 const exec = require('child_process').exec;
+const logger = require('tracer').colorConsole(config.log);
+
+process.env.PG_CONNECTION_STRING = 'postgres://' + config.database.user + ':' + config.database.password + '@' + config.database.host + '/' + config.database.database;;
+
 app.get('/read_sensor', function (req, res) {
-  exec('./read_sensor.py', (e, stdout, stderr)=> {
+  exec('./read_sensor.py', (e, stdout, stderr) => {
     if (e instanceof Error) {
-        console.error(e);
-        throw e;
+      console.error(e);
+      throw e;
     }
     res.setHeader('Content-Type', 'application/json');
     res.send(stdout);
   });
 })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(config.server.port, function () {
+  logger.debug('Example app listening on port 3000!')
 })
